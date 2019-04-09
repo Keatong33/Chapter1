f7247c19d9d046bb600bc4c3ce645a8bc98d23f6
Greenwalt <KG155781@davenportschools.org>
2019-04-09 12:36:11 -0500

    Initial commit

---
+++ src/TableAndChairs.java
@@ -0,0 +1,23 @@
+public class TableAndChairs {
+    //Author: Keaton
+    //Date: 04/09/2019
+    //Project: Tables and Chairs
+
+    public static void main(String[] args) {
+
+         System.out.println("Building Some Tables And Chairs");
+         System.out.println("\n\n\n\n");
+
+         System.out.println("C                                       C");
+         System.out.println("H                                       H");
+         System.out.println("A                                       A");
+         System.out.println("I             TABLETABLETABLETABLE      I");
+         System.out.println("R               L                       R");
+         System.out.println("CHAIRCHAIR      E              CHAIRCHAIR");
+         System.out.println("C        C      G                       C");
+         System.out.println("H        H      L                       H");
+         System.out.println("A        A      E                       A");
+         System.out.println("I               G                       I");
+         System.out.println("R        R      !                       R");
+    }
+}
