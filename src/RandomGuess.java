a7dff23e3b34542566fe3073b0ab7015d6bdb63f
Greenwalt <KG155781@davenportschools.org>
2019-04-10 13:25:14 -0500

    update

--- src/RandomGuess.java
+++ src/RandomGuess.java
@@ -1,2 +1,9 @@
+import javax.swing
 public class RandomGuess {
+    public static void main(String [] args)
+    {
+        JOptionPane.showmessageDialog( parentComponent null, message:"The number is " +
+            ((int) (Math.random() * 10));
+    }
+
 }
